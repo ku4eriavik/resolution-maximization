ecb054b7e4e31431466637aba50998f80ad3fad3
ku4eriavik <43913782+ku4eriavik@users.noreply.github.com>
2025-04-02 03:52:58 +0300

    Implemented Region

--- tools.py
+++ tools.py
@@ -15,3 +15,16 @@ def create_surface_from_config(config_path: Path):
 
         surface = Surface(points=np.asarray(points, dtype=np.float32))
         return surface
+
+
+def create_region_from_config(config_path: Path):
+    assert config_path.exists(), f'Region D config file {str(config_path)} does not exist.'
+
+    with open(str(config_path), 'r') as json_file:
+        data = load(json_file)
+        x_range = data.get('x_range', None)
+        y_range = data.get('y_range', None)
+        assert x_range is not None, 'Parameter x_range has to be set.'
+        assert y_range is not None, 'Parameter y_range has to be set.'
+
+        return x_range, y_range
